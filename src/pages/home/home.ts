e2dc2ee3a584fc7436e0b2619e71db7480f1ddee
Gonzalo <g.orellanacanales@uandresbello.edu>
2019-12-13 16:40:20 -0300

    13-12-2019

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { NavController, Item, LoadingController } from 'ionic-angular';
 import 'rxjs/add/operator/map'
 import { AuthJsonDataProvider } from '../../providers/auth-json-data/auth-json-data';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -28,38 +29,60 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     public authJsonDataProvider: AuthJsonDataProvider,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    public http: HttpClient
     ) {
-
 
   }
 
   ionViewDidLoad(){
-    this.login()
+    //this.login()
   }
 
   login(){
-    //if(this.email && this.password){
+    if(this.email && this.password){
       this.authJsonDataProvider.getUsers().subscribe(res=>{
       this.usuarios = res;
-      console.log(this.usuarios);
+      //console.log(this.usuarios);
+      console.log(this.email, this.password);
       for(var n = 0; n < this.usuarios.length; n++){
-        console.log(this.usuarios[n])
-        /*if(this.usuarios[n].email === this.email && this.usuarios[n].password === this.password){
+        if(this.email === this.usuarios[n].email && this.password === this.usuarios[n].login.password){
           console.log('Se ha logueado: '+this.usuarios[n].name.first);
+          //console.log(this.usuarios[n]);
+          this.userDataBase = this.usuarios[n];
+          console.log(this.userDataBase);
+          this.authState = true;
           break
-        }*/
+        }else{
+          if(n = this.usuarios.length){
+            alert('Usuario no registrado')
+          }
+        }
       }
       }),
       (error) =>{
         console.error(error);
       }
-    //}else{
-      //alert('Faltan credenciales')
-    //}
-    
+    }else{
+      alert('Faltan credenciales')
+    }
   }
 
-
+  postDatos(){
+    let datos = { nombre:'Edu',email:'edu.revilla.vaquero@gmail.com'}
+    
+    let options = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+   var url = './assets/auth.json';
+   return new Promise(resolve => {
+    this.http.post(url,JSON.stringify(datos),options)
+       .subscribe(data => {
+         resolve(data);
+        });
+   });
+  }
 
 }
